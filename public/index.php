b086a795e9e2d29d18efbc7bccfb7863f03f293a
Anyon <zoujingli@qq.com>
2017-02-09 21:14:12 -0500

    还原config到application目录

--- public/index.php
+++ public/index.php
@@ -3,8 +3,5 @@
 // 定义应用目录
 define('APP_PATH', dirname(__DIR__) . '/application/');
 
-// 定义配置目录
-define('CONF_PATH', dirname(__DIR__) . '/config/');
-
 // 加载框架引导文件
 require dirname(__DIR__) . '/thinkphp/start.php';
