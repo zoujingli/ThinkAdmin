21325a604c0d6969a2f1559ed986a52f8c8c58ff
邹景立 <zoujingli@qq.com>
2017-02-13 16:29:38 +0800

    更新布局名称

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -3,10 +3,14 @@
 namespace app\admin\controller;
 
 use controller\BasicAdmin;
+use think\Db;
 
 class Index extends BasicAdmin {
 
     public function index() {
+        $version = Db::query('select version() as ver');
+        $version = array_pop($version);
+        $this->assign('mysql_ver', $version['ver']);
         return view();
     }
 
