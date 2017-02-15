c159ef43f14f3b36361d8088c29f83b3cff5cc4e
邹景立 <zoujingli@qq.com>
2017-02-15 18:06:44 +0800

    增加后台配置控制器

---
+++ application/admin/controller/Config.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\admin\controller;
+
+use controller\BasicAdmin;
+
+/**
+ * 后台参数配置控制器
+ * Class Config
+ * @package app\admin\controller
+ * @author Anyon <zoujingli@qq.com>
+ * @date 2017/02/15 18:05
+ */
+class Config extends BasicAdmin {
+
+    public function index() {
+
+    }
+
+}
