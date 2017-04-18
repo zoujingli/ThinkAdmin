9001871d7d258332d824936d39260b3f590b1a96
邹景立 <zoujingli@qq.com>
2017-04-18 17:56:07 +0800

    [更新]修改系统节点过滤

--- application/admin/model/NodeModel.php
+++ application/admin/model/NodeModel.php
@@ -89,8 +89,13 @@ class NodeModel {
             $alias["{$vo['node']}"] = $vo;
         }
         $nodes = [];
+        $ignore = [
+            'index',
+            'wechat/api', 'wechat/notify',
+            'admin/plugs', 'admin/login', 'admin/index',
+        ];
         foreach (self::getNodeTree(APP_PATH) as $thr) {
-            foreach (['admin/plugs', 'admin/login', 'admin/index', 'store/api', 'index'] as $str) {
+            foreach ($ignore as $str) {
                 if (stripos($thr, $str) === 0) {
                     continue 2;
                 }
