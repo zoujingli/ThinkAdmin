57e86528248311522ad4ff315a0bf9dbc5324c38
邹景立 <zoujingli@qq.com>
2017-04-21 15:46:34 +0800

    [更新]微信关键字管理

--- application/wechat/controller/Review.php
+++ application/wechat/controller/Review.php
@@ -11,14 +11,52 @@
 // | github开源项目：https://github.com/zoujingli/Think.Admin
 // +----------------------------------------------------------------------
 
-
 namespace app\wechat\controller;
 
-class Review {
+use think\Controller;
+use think\Db;
 
+class Review extends Controller {
+
+    /**
+     * 显示手机预览
+     * @return string
+     */
     public function index() {
-        
-        return '222';
+        $get = $this->request->get();
+        // 内容
+        $content = $this->request->get('content', '');
+        $this->assign('content', $content);
+        // 类型
+        $type = $this->request->get('type', 'text');
+        $this->assign('type', $type);
+        // 图文处理
+        if ($type === 'news' && is_numeric($content) && !empty($content)) {
+            $news = News::get($content, ['appid' => $this->real_appid]);
+            $this->assign('articles', $news['articles']);
+        }
+        // 文章预览
+        if ($type === 'article' && is_numeric($content) && !empty($content)) {
+            $article = Db::name('WechatNewsArticle')->where('id', $content)->find();
+            $this->assign('vo', $article);
+        }
+        $this->assign($get);
+        // 渲染模板并显示
+        return view();
+    }
+
+    /**
+     * 微信图片显示
+     */
+    public function wechatImage() {
+        $url = input('get.url', '');
+        $filename = 'upload/tmp/' . join('/', str_split(md5($url), 16));
+        $realfile = ROOT_PATH . 'public/' . $filename;
+        file_exists(dirname($realfile)) || mkdir(dirname($realfile), 0755, true);
+        if (!file_exists($realfile)) {
+            file_put_contents($realfile, file_get_contents($url));
+        }
+        $this->redirect(pathinfo($this->requestbaseFile(true), PATHINFO_DIRNAME) . '/' . $filename);
     }
 
 }
