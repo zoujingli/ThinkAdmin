64a60dd73120be50b6dffd1a2b24bcd7014c37c2
Anyon <Anyon@windows10.microdone.cn>
2017-04-25 11:37:36 +0800

    [更新]调整session_name规则

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
 // +----------------------------------------------------------------------
 
 /* SESSION会话名称 */
-session_name(md5(__FILE__));
+session_name('sess' . substr(md5(__FILE__)), 0, 8);
 
 /* 定义应用目录 */
 define('APP_PATH', __DIR__ . '/application/');
 
 /* 定义Runtime运行目录 */
-define('RUNTIME_PATH', __DIR__ . '/static/~runtime/');
+define('RUNTIME_PATH', __DIR__ . '/static/runtime/');
 
 /* 加载框架引导文件 */
 require __DIR__ . '/thinkphp/start.php';
