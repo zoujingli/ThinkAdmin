c958252c02149a8e7337ed83f2c38f453f55a388
Anyon <zoujingli@qq.com>
2017-07-06 15:33:38 +0800

    [更新]代码优雅处理

--- application/admin/controller/Log.php
+++ application/admin/controller/Log.php
@@ -38,14 +38,15 @@ class Log extends BasicAdmin {
      */
     public function index() {
         $this->title = '系统操作日志';
-        $this->assign('actions', Db::name($this->table)->group('action')->column('action'));
-        $db = Db::name($this->table)->order('id desc');
         $get = $this->request->get();
+        $actions = Db::name($this->table)->group('action')->column('action');
+        $db = Db::name($this->table)->order('id desc');
         foreach (['action', 'content', 'username'] as $key) {
             if (isset($get[$key]) && $get[$key] !== '') {
                 $db->where($key, 'like', "%{$get[$key]}%");
             }
         }
+        $this->assign('actions', $actions);
         return parent::_list($db);
     }
 
@@ -58,7 +59,7 @@ class Log extends BasicAdmin {
         foreach ($data as &$vo) {
             $result = $ip->btreeSearch($vo['ip']);
             $vo['isp'] = isset($result['region']) ? $result['region'] : '';
-            $vo['isp'] = str_replace(['|0|0|0|0', '|'], ['', ' '], $vo['isp']);
+            $vo['isp'] = str_replace(['|0|0|0|0', '0', '|'], ['', '', ' '], $vo['isp']);
         }
     }
 
@@ -67,9 +68,9 @@ class Log extends BasicAdmin {
      */
     public function del() {
         if (DataService::update($this->table)) {
-            $this->success("日志删除成功！", '');
+            $this->success("日志删除成功!", '');
         }
-        $this->error("日志删除失败，请稍候再试！");
+        $this->error("日志删除失败, 请稍候再试!");
     }
 
 }
