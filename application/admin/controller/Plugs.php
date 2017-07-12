355e730fa4c148d71b5460d05910c74e65f156ba
Anyon <zoujingli@qq.com>
2017-07-12 17:24:43 +0800

    [更新]增加文件上传部分文件类型过滤

--- application/admin/controller/Plugs.php
+++ application/admin/controller/Plugs.php
@@ -62,16 +62,22 @@ class Plugs extends BasicAdmin
      */
     public function upload()
     {
-        if ($this->request->isPost()) {
-            $md5s = str_split($this->request->post('md5'), 16);
-            if (($info = $this->request->file('file')->move('static' . DS . 'upload' . DS . $md5s[0], $md5s[1], true))) {
-                $filename = join('/', $md5s) . '.' . $info->getExtension();
-                if (($site_url = FileService::getFileUrl($filename, 'local'))) {
-                    return json(['data' => ['site_url' => $site_url], 'code' => 'SUCCESS']);
-                }
+        if (!$this->request->isPost()) {
+            return json(['code' => 'ERROR', '文件上传失败']);
+        }
+        $file = $this->request->file('file');
+        $ext = pathinfo($file->getInfo('name'), PATHINFO_EXTENSION);
+        if (in_array($ext, ['php', 'bat', 'cmd', 'sh', 'exe'])) {
+            return json(['code' => 'ERROR', 'msg' => "禁止上传{$ext}文件"]);
+        }
+        $md5s = str_split($this->request->post('md5'), 16);
+        if (($info = $file->move('static' . DS . 'upload' . DS . $md5s[0], $md5s[1], true))) {
+            $filename = join('/', $md5s) . '.' . $info->getExtension();
+            if (($site_url = FileService::getFileUrl($filename, 'local'))) {
+                return json(['data' => ['site_url' => $site_url], 'code' => 'SUCCESS', 'msg' => '文件上传成功']);
             }
         }
-        return json(['code' => 'ERROR']);
+        return json(['code' => 'ERROR', '文件上传失败']);
     }
 
     /**
