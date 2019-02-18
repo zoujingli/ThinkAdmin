4a80dc8ffe1681099bb760771e1759cf600810c2
邹景立 <zoujingli@qq.com>
2019-02-18 14:56:48 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -18,11 +18,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'ThinkAdmin',
+    'database'        => 'admin_v2',
     // 用户名
-    'username'        => 'ThinkAdmin',
+    'username'        => 'admin_v2',
     // 密码
-    'password'        => 'ThinkAdmin',
+    'password'        => 'FbYBHcWKr2',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
