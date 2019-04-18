2b8661c3578ba6f57f9acb119794b3e31fa44453
Anyon <zoujingli@qq.com>
2019-04-18 13:33:09 +0800

    Update log.php

--- config/log.php
+++ config/log.php
@@ -20,7 +20,7 @@ return [
     // 日志每10兆分割文件
     'file_size'   => 10485760,
     // 设置记录目录的类型
-    'level'       => ['error'],
+    // 'level'       => ['error'],
     // 日志类型分别写入文件
     'apart_level' => ['error', 'sql'],
 ];
