73540d3ada091a57b3734b738755debbce31c43a
Anyon <zoujingli@qq.com>
2019-08-01 14:31:46 +0800

    Update MichatService.php

--- application/service/service/MichatService.php
+++ application/service/service/MichatService.php
@@ -1,12 +1,28 @@
 <?php
 
+// +----------------------------------------------------------------------
+// | ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2019 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://demo.thinkadmin.top
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/zoujingli/ThinkAdmin
+// | github 代码仓库：https://github.com/zoujingli/ThinkAdmin
+// +----------------------------------------------------------------------
 
 namespace app\service\service;
-
 
 use library\tools\Http;
 use think\Exception;
 
+/**
+ * 小米消息服务
+ * Class MichatService
+ * @package app\service\service
+ */
 class MichatService
 {
     const URI = 'https://mimc.chat.xiaomi.net';
@@ -18,7 +34,6 @@ class MichatService
     const BIZ_TYPE_BIN_FILE = 'BIN_FILE';
     const BIZ_TYPE_AUDIO_FILE = 'AUDIO_FILE';
     const MSG_TYPE_BASE64 = 'base64';
-
 
     /**
      * 给指定账号推送消息内容
