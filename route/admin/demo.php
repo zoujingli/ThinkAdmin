076e2ec62dd0703a558092f1e45abfab2c18c648
Anyon <zoujingli@qq.com>
2019-11-21 11:14:32 +0800

    修改测试路由配置

--- route/admin/demo.php
+++ route/admin/demo.php
@@ -13,34 +13,33 @@
 // | github 代码仓库：https://github.com/zoujingli/ThinkAdmin
 // +----------------------------------------------------------------------
 
-/*  演示环境禁止操作路由绑定 */
-if (in_array(request()->rootDomain(), ['ctolog.com', 'thinkadmin.top'])) {
-    $app = app();
-    $app->route->post('admin/user/pass', function () {
+/* 演示环境禁止操作路由绑定 */
+if (stripos(app()->request->rootDomain(), 'thinkadmin.top') !== false) {
+    app()->route->post('index/pass', function () {
         return json(['code' => 0, 'info' => '演示环境禁止修改用户密码！']);
     });
-    $app->route->post('admin/index/pass', function () {
-        return json(['code' => 0, 'info' => '演示环境禁止修改用户密码！']);
-    });
-    $app->route->post('admin/config/config', function () {
+    app()->route->post('config/system', function () {
         return json(['code' => 0, 'info' => '演示环境禁止修改系统配置！']);
     });
-    $app->route->post('admin/config/storage', function () {
+    app()->route->post('config/storage', function () {
         return json(['code' => 0, 'info' => '演示环境禁止修改系统配置！']);
     });
-    $app->route->post('admin/menu/index', function () {
+    app()->route->post('menu/index', function () {
         return json(['code' => 0, 'info' => '演示环境禁止给菜单排序！']);
     });
-    $app->route->post('admin/menu/add', function () {
+    app()->route->post('menu/add', function () {
         return json(['code' => 0, 'info' => '演示环境禁止添加菜单！']);
     });
-    $app->route->post('admin/menu/edit', function () {
+    app()->route->post('menu/edit', function () {
         return json(['code' => 0, 'info' => '演示环境禁止编辑菜单！']);
     });
-    $app->route->post('admin/menu/state', function () {
+    app()->route->post('menu/state', function () {
         return json(['code' => 0, 'info' => '演示环境禁止禁用菜单！']);
     });
-    $app->route->post('admin/menu/remove', function () {
+    app()->route->post('menu/remove', function () {
         return json(['code' => 0, 'info' => '演示环境禁止删除菜单！']);
+    });
+    app()->route->post('user/pass', function () {
+        return json(['code' => 0, 'info' => '演示环境禁止修改用户密码！']);
     });
 }
