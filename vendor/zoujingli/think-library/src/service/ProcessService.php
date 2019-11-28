646c330c8d7462ebfcdee998aa5fcd0949926434
Anyon <zoujingli@qq.com>
2019-11-28 11:22:34 +0800

    ComposerUpdate

--- vendor/zoujingli/think-library/src/service/ProcessService.php
+++ vendor/zoujingli/think-library/src/service/ProcessService.php
@@ -55,7 +55,7 @@ class ProcessService extends Service
         if ($this->iswin()) {
             $this->exec(__DIR__ . "/bin/console.exe {$command}");
         } else {
-            $this->exec("{$command} &");
+            $this->exec("{$command} > /dev/null &");
         }
         return $this;
     }
