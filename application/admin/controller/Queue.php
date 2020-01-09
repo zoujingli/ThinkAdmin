dcb5cd69de48e5477aa90ee7601ebe2b771e4a86
Anyon <zoujingli@qq.com>
2020-01-09 10:25:35 +0800

    ComposerUpdate 修改系统模块

--- application/admin/controller/Queue.php
+++ application/admin/controller/Queue.php
@@ -16,7 +16,9 @@
 namespace app\admin\controller;
 
 use library\Controller;
+use library\service\ProcessService;
 use think\Console;
+use think\Db;
 use think\exception\HttpResponseException;
 
 /**
@@ -45,13 +47,14 @@ class Queue extends Controller
     public function index()
     {
         if (session('user.username') === 'admin') try {
-            $this->cmd = 'php ' . env('root_path') . 'think xtask:start';
             $this->message = Console::call('xtask:state')->fetch();
+            $this->command = ProcessService::instance()->think('xtask:start');
+            $this->listen = preg_match('/process.*?\d+.*?running/', $this->message, $attr);
         } catch (\Exception $exception) {
             $this->message = $exception->getMessage();
         }
         $this->title = '系统任务管理';
-        $this->iswin = PATH_SEPARATOR === ';';
+        $this->iswin = ProcessService::instance()->iswin();
         $query = $this->_query($this->table)->dateBetween('create_at,start_at,end_at');
         $query->like('title,preload')->equal('status')->order('id desc')->page();
     }
@@ -71,10 +74,15 @@ class Queue extends Controller
      * WIN开始监听任务
      * @auth true
      */
-    public function processStart()
+    public function start()
     {
         try {
-            $this->success(nl2br(Console::call('xtask:start')->fetch()));
+            $message = nl2br(Console::call('xtask:start')->fetch());
+            if (preg_match('/process.*?\d+/', $message, $attr)) {
+                $this->success('任务监听主进程启动成功！');
+            } else {
+                $this->error($message);
+            }
         } catch (HttpResponseException $exception) {
             throw $exception;
         } catch (\Exception $e) {
@@ -86,14 +94,38 @@ class Queue extends Controller
      * WIN停止监听任务
      * @auth true
      */
-    public function processStop()
+    public function stop()
     {
         try {
-            $this->success(nl2br(Console::call('xtask:stop')->fetch()));
+            $message = nl2br(Console::call('xtask:stop')->fetch());
+            if (stripos($message, 'succeeded')) {
+                $this->success('停止任务监听主进程成功！');
+            } elseif (stripos($message, 'finish')) {
+                $this->success('没有找到需要停止的进程！');
+            } else {
+                $this->error($message);
+            }
         } catch (HttpResponseException $exception) {
             throw $exception;
         } catch (\Exception $e) {
             $this->error($e->getMessage());
+        }
+    }
+
+    /**
+     * 清理3天前的记录
+     * @auth true
+     * @throws \think\Exception
+     * @throws \think\exception\PDOException
+     */
+    public function clear()
+    {
+        $map = [['time', '<', strtotime('-3days')]];
+        $result = Db::name($this->table)->where($map)->delete();
+        if ($result !== false) {
+            $this->success('成功清理3天前的任务记录！');
+        } else {
+            $this->error('清理3天前的任务记录失败！');
         }
     }
 
