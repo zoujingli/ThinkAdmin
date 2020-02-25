0beb179fdf9341f55fafd95910bb25640cae7b38
Anyon <zoujingli@qq.com>
2020-02-25 15:57:54 +0800

    ComposerUpdate

--- vendor/zoujingli/think-library/src/common.php
+++ vendor/zoujingli/think-library/src/common.php
@@ -36,7 +36,7 @@ if (!function_exists('p')) {
     function p($data, $force = false, $file = null)
     {
         if (is_null($file)) $file = env('runtime_path') . date('Ymd') . '.txt';
-        $str = (is_string($data) ? $data : (is_array($data) || is_object($data)) ? print_r($data, true) : var_export($data, true)) . PHP_EOL;
+        $str = (is_string($data) ? $data : ((is_array($data) || is_object($data)) ? print_r($data, true) : var_export($data, true))) . PHP_EOL;
         $force ? file_put_contents($file, $str) : file_put_contents($file, $str, FILE_APPEND);
     }
 }
