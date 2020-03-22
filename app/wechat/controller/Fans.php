55101d845e7ee713ffb818e772a4fa179010f52d
邹景立 <zoujingli@qq.com>
2020-03-22 22:20:39 +0800

    Update Fans.php

--- app/wechat/controller/Fans.php
+++ app/wechat/controller/Fans.php
@@ -74,8 +74,6 @@ class Fans extends Controller
         try {
             $code = sysqueue('同步微信用户数据', "xadmin:fansall", 1, [], 0);
             $this->success('创建任务成功，请等待完成！', $code);
-        } catch (HttpResponseException $exception) {
-            throw $exception;
         } catch (Exception $exception) {
             $queue = $exception->getData();
             if (isset($queue['code'])) {
@@ -83,6 +81,8 @@ class Fans extends Controller
             } else {
                 $this->error($exception->getMessage());
             }
+        } catch (HttpResponseException $exception) {
+            throw $exception;
         } catch (\Exception $exception) {
             $this->error("创建任务失败，{$exception->getMessage()}");
         }
