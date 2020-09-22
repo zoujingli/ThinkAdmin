5692b7b55f787f91b34fd76232e68bd785c31ce6
Anyon <zoujingli@qq.com>
2020-09-22 19:06:42 +0800

    Update NewsItem.php

--- app/data/controller/NewsItem.php
+++ app/data/controller/NewsItem.php
@@ -95,9 +95,9 @@ class NewsItem extends Controller
         if ($this->request->isGet()) {
             $query = $this->app->db->name('DataNewsMark')->where(['deleted' => 0, 'status' => 1]);
             $this->mark = $query->order('sort desc,id desc')->select()->toArray();
-            $data['mark'] = mark_str2arr($data['mark']);
+            $data['mark'] = mark_string_array($data['mark']);
         } else {
-            $data['mark'] = mark_arr2str($data['mark'] ?? []);
+            $data['mark'] = mark_array_string($data['mark'] ?? []);
         }
     }
 
