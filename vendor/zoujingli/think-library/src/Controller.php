491adc1393f1e73e3e3288e72264473baa79c0b5
Anyon <zoujingli@qq.com>
2020-09-27 10:57:31 +0800

    ComposerUpdate

--- vendor/zoujingli/think-library/src/Controller.php
+++ vendor/zoujingli/think-library/src/Controller.php
@@ -236,10 +236,10 @@ abstract class Controller extends \stdClass
     /**
      * 快捷输入并验证（ 支持 规则 # 别名 ）
      * @param array $rules 验证规则（ 验证信息数组 ）
-     * @param string $type 输入方式 ( post. 或 get. )
+     * @param string|array $type 输入方式 ( post. 或 get. )
      * @return array
      */
-    protected function _vali(array $rules, string $type = '')
+    protected function _vali(array $rules, $type = '')
     {
         return ValidateHelper::instance()->init($rules, $type);
     }
--- vendor/zoujingli/think-library/src/Library.php
+++ vendor/zoujingli/think-library/src/Library.php
@@ -41,7 +41,7 @@ class Library extends Service
     /**
      * 扩展库版本号
      */
-    const VERSION = '6.0.14';
+    const VERSION = '6.0.15';
 
     /**
      * 启动服务
