58c4b35b2a5dba1dc1d6cfff4c796e9703df0582
Anyon <zoujingli@qq.com>
2020-10-12 11:45:30 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -35,7 +35,7 @@ return [
     // 禁止URL访问的应用列表（自动多应用模式有效）
     'deny_app_list'    => [],
     // 异常页面的模板文件
-    'exception_tmpl'   => app()->getAppPath() . 'admin/view/error.tpl',
+    'exception_tmpl'   => app()->getBasePath() . 'admin/view/error.tpl',
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
