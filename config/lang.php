b89118feb70d1d8488d8cfd62547c0b04f5d9435
邹景立 <zoujingli@qq.com>
2021-01-29 16:22:47 +0800

    Update lang.php

--- config/lang.php
+++ config/lang.php
@@ -25,9 +25,9 @@ return [
     // 多语言自动侦测变量名
     'detect_var'      => 'lang',
     // 多语言 Cookie 变量
-    'cookie_var'      => 'think_lang',
+    'cookie_var'      => 'lang',
     // 多语言 Header 变量
-    'header_var'      => 'think-lang',
+    'header_var'      => 'lang',
     // 使用 Cookie 记录
     'use_cookie'      => true,
     // 是否支持语言分组
