31db814094a520dde087763727c4cd3cd57c83ae
邹景立 <zoujingli@qq.com>
2021-03-17 12:08:30 +0800

    增加兼容模式

--- app/data/service/UserTransferService.php
+++ app/data/service/UserTransferService.php
@@ -17,8 +17,9 @@ class UserTransferService extends Service
      */
     protected $types = [
         'wechat_wallet'  => '转账到我的微信零钱',
-        'wechat_qrcode'  => '线下微信收款码转账',
-        'alipay_qrcode'  => '线下支付宝收款码转账',
+        'wechat_banks'   => '转账到我的银行卡账户',
+        'wechat_qrcode'  => '线下转账到微信收款码',
+        'alipay_qrcode'  => '线下转账到支付宝收款码',
         'alipay_account' => '线下转账到支付宝账户',
         'transfer_banks' => '线下转账到银行卡账户',
     ];
