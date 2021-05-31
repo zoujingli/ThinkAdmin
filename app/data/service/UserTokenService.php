814720d56e3c09af51f52ee214a35c54d4bf1b30
Anyon <zoujingli@qq.com>
2021-05-31 20:14:55 +0800

    Update UserTokenService.php

--- app/data/service/UserTokenService.php
+++ app/data/service/UserTokenService.php
@@ -82,9 +82,10 @@ class UserTokenService extends Service
     public function token(int $uuid, string $type): array
     {
         // 清理无效认证数据
-        $map1 = [['uid', '=', $uuid], ['type', '=', $type]];
-        $map2 = [['time', '<', $time = time()], ['token', '<>', 'token']];
-        $this->app->db->name('DataUserToken')->whereOr([$map1, $map2])->delete();
+        $map0 = [['token', '<>', 'token']];
+        $map1 = [['time', '<', $time = time()]];
+        $map2 = [['type', '=', $type], ['uid', '=', $uuid]];
+        $this->app->db->name('DataUserToken')->where($map0)->whereOr([$map1, $map2])->delete();
         // 创建新的认证数据
         do $map = ['type' => $type, 'token' => md5(uniqid() . rand(100, 999))];
         while ($this->app->db->name('DataUserToken')->where($map)->count() > 0);
