0fa2414549235228565a158d59e802a77a4738d3
邹景立 <zoujingli@qq.com>
2021-06-28 09:55:36 +0800

    去除 vendor 目录，需要自行安装

--- vendor/topthink/framework/tests/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-include __DIR__.'/../src/helper.php';
--- vendor/topthink/think-orm/stubs/load_stubs.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!\class_exists('think\Exception')) {
-    require __DIR__ . '/Exception.php';
-}
-
-if (!\class_exists('think\Facade')) {
-    require __DIR__ . '/Facade.php';
-}
