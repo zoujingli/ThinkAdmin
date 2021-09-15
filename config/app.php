ba70d57b8d544e68f4049d11c895efe02082cc46
邹景立 <zoujingli@qq.com>
2021-09-15 16:52:37 +0800

    修改配置文件

--- config/app.php
+++ config/app.php
@@ -23,8 +23,6 @@ return [
     'with_route'       => true,
     // 超级用户账号
     'super_user'       => 'admin',
-    // 默认应用
-    'default_app'      => 'index',
     // 默认时区
     'default_timezone' => 'Asia/Shanghai',
     // 应用映射（自动多应用模式有效）
--- config/route.php
+++ config/route.php
@@ -43,6 +43,8 @@ return [
     'request_cache_expire'  => null,
     // 全局请求缓存排除规则
     'request_cache_except'  => [],
+    // 默认应用
+    'default_app'           => 'index',
     // 默认控制器名
     'default_controller'    => 'Index',
     // 默认操作名
