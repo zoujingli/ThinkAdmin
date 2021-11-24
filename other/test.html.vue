83965c11dd473a1b43d2f088d4df9d9e6a54820f
Anyon <zoujingli@qq.com>
2021-11-24 14:08:33 +0800

    增加其他测试案例

---
+++ other/test.html.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">Hello {{ who }}</div>
+</template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      who: 'xxx'
+    }
+  },
+  created: function () {
+    console.log(234123)
+  }
+}
+</script>
+
+<style>
+.hello {
+  background-color: #ffe;
+}
+</style>
