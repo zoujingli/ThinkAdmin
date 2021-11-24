e51c21951eddf4a6583889e4ef602f0261b469e3
邹景立 <zoujingli@qq.com>
2021-11-24 22:23:00 +0800

    增加 Less 支持

--- static/template/layout.vue
+++ static/template/layout.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="full_screen">
-    <router-view></router-view>
-  </div>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="280px"></el-aside>
+      <el-container>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
