d7ec24548115314a2843f8aef51202fa91a7dfdd
邹景立 <zoujingli@qq.com>
2021-11-26 22:38:06 +0800

    Update layout.vue

--- static/template/layout.vue
+++ static/template/layout.vue
@@ -26,39 +26,33 @@
             <el-menu default-active="2">
               <el-sub-menu index="1">
                 <template #title>
-                  <el-icon>
-                    <location/>
-                  </el-icon>
+<!--                  <el-icon>-->
+<!--                    <location></location>-->
+<!--                  </el-icon>-->
                   <span>Navigator One</span>
                 </template>
-                <el-menu-item-group title="Group One">
-                  <el-menu-item index="1-1">item one</el-menu-item>
-                  <el-menu-item index="1-2">item one</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="Group Two">
-                  <el-menu-item index="1-3">item three</el-menu-item>
-                </el-menu-item-group>
-                <el-sub-menu index="1-4">
-                  <template #title>item four</template>
-                  <el-menu-item index="1-4-1">item one</el-menu-item>
-                </el-sub-menu>
+
+                <el-menu-item index="1-1">item one</el-menu-item>
+                <el-menu-item index="1-2">item one</el-menu-item>
+                <el-menu-item index="1-3">item three</el-menu-item>
+
               </el-sub-menu>
               <el-menu-item index="2">
-                <el-icon>
-                  <icon-menu/>
-                </el-icon>
+<!--                <el-icon>-->
+<!--                  <icon-menu></icon-menu>-->
+<!--                </el-icon>-->
                 <span>Navigator Two</span>
               </el-menu-item>
-              <el-menu-item index="3" disabled>
-                <el-icon>
-                  <document/>
-                </el-icon>
+              <el-menu-item index="3">
+<!--                <el-icon>-->
+<!--                  <document></document>-->
+<!--                </el-icon>-->
                 <span>Navigator Three</span>
               </el-menu-item>
               <el-menu-item index="4">
-                <el-icon>
-                  <setting/>
-                </el-icon>
+<!--                <el-icon>-->
+                <!--                  <setting></setting>-->
+                <!--                </el-icon>-->
                 <span>Navigator Four</span>
               </el-menu-item>
             </el-menu>
