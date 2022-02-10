b03cb6fc1dc3403532c91a45f63701d33711e34c
Anyon <zoujingli@qq.com>
2022-02-10 13:47:43 +0800

    修改注释

--- public/router.php
+++ public/router.php
@@ -3,7 +3,7 @@
 // +----------------------------------------------------------------------
 // | ThinkAdmin
 // +----------------------------------------------------------------------
-// | 版权所有 2014~2021 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// | 版权所有 2014~2022 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
 // +----------------------------------------------------------------------
 // | 官方网站: https://thinkadmin.top
 // +----------------------------------------------------------------------
@@ -14,9 +14,9 @@
 // | github 代码仓库：https://github.com/zoujingli/ThinkAdmin
 // +----------------------------------------------------------------------
 
-if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
+if (is_file($_SERVER['DOCUMENT_ROOT'] . $_SERVER['SCRIPT_NAME'])) {
     return false;
 } else {
-    $_SERVER["SCRIPT_FILENAME"] = __DIR__ . '/index.php';
-    require __DIR__ . "/index.php";
+    $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/index.php';
+    require $_SERVER['SCRIPT_FILENAME'];
 }
