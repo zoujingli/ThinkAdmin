77c1bd7b85a5d929d70405e0da114124958c39a5
Anyon <zoujingli@qq.com>
2022-04-15 14:28:43 +0800

    Update DataNewsMark.php

--- app/data/model/DataNewsMark.php
+++ app/data/model/DataNewsMark.php
@@ -5,11 +5,18 @@ namespace app\data\model;
 use think\admin\Model;
 
 /**
- *
  * Class DataNewsMark
  * @package app\data\model
  */
 class DataNewsMark extends Model
 {
-
+    /**
+     * 格式化创建时间
+     * @param string $value
+     * @return string
+     */
+    public function getCreateAtAttr(string $value): string
+    {
+        return format_datetime($value);
+    }
 }
