608421a84a5681e4d53e7eb5a7261581d19d83f4
Anyon <zoujingli@qq.com>
2022-04-15 14:21:20 +0800

    修改系统通知

--- app/data/model/BaseUserMessage.php
+++ app/data/model/BaseUserMessage.php
@@ -11,5 +11,13 @@ use think\admin\Model;
  */
 class BaseUserMessage extends Model
 {
-
+    /**
+     * 格式化创建时间
+     * @param string $value
+     * @return string
+     */
+    public function getCreateAtAttr(string $value): string
+    {
+        return format_datetime($value);
+    }
 }
