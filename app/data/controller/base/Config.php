c379d6224e3be978cfb1bdb626b2a8b3a52014ac
邹景立 <zoujingli@qq.com>
2022-04-16 22:28:23 +0800

    修改页面管理

--- app/data/controller/base/Config.php
+++ app/data/controller/base/Config.php
@@ -11,14 +11,6 @@ use think\admin\Controller;
  */
 class Config extends Controller
 {
-    /**
-     * 页面类型
-     * @var array
-     */
-    protected $pageTypes = [
-        '关于我们' => '关于我们',
-        '用户协议' => '用户协议',
-    ];
 
     /**
      * 微信小程序配置
@@ -30,8 +22,9 @@ class Config extends Controller
      */
     public function wxapp()
     {
+        $this->skey = 'wxapp';
         $this->title = '微信小程序配置';
-        $this->__sysconf('wxapp');
+        $this->__sysdata('wxapp');
     }
 
     /**
@@ -44,83 +37,9 @@ class Config extends Controller
      */
     public function cropper()
     {
-        $this->title = '邀请二维码设置';
         $this->skey = 'cropper';
+        $this->title = '邀请二维码设置';
         $this->__sysdata('cropper');
-    }
-
-    /**
-     * 内容页面管理
-     * @auth true
-     * @menu true
-     */
-    public function pageHome()
-    {
-        $this->title = '内容页面管理';
-        $this->fetch('page_home');
-    }
-
-    /**
-     * 内容页面编辑
-     * @auth true
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
-     */
-    public function pageEdit()
-    {
-        $this->skey = input('type') ?: $this->pageTypes[0];
-        $this->title = '编辑' . $this->pageTypes[$this->skey] ?? '';
-        $this->__sysdata('page_form', 'javascript:history.back()');
-    }
-
-    /**
-     * 首页推荐位管理
-     * @menu true
-     * @auth true
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
-     */
-    public function iconHome()
-    {
-        $this->skey = 'IconHome';
-        $this->title = '首页推荐位管理';
-        $this->__sysdata('slider');
-    }
-
-    /**
-     * 首页轮播图片
-     * @menu true
-     * @auth true
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
-     */
-    public function sliderHome()
-    {
-        $this->skey = 'SliderHome';
-        $this->title = '首页轮播图片';
-        $this->__sysdata('slider');
-    }
-
-    /**
-     * 显示并保存配置
-     * @param string $template 模板文件名称
-     * @throws \think\db\exception\DataNotFoundException
-     * @throws \think\db\exception\DbException
-     * @throws \think\db\exception\ModelNotFoundException
-     */
-    private function __sysconf(string $template)
-    {
-        if ($this->request->isGet()) {
-            $this->fetch($template);
-        }
-        if ($this->request->isPost()) {
-            $data = $this->request->post();
-            foreach ($data as $k => $v) sysconf($k, $v);
-            $this->success('配置保存成功！');
-        }
     }
 
     /**
