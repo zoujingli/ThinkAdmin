a912af27db1094cfb12dff7208c3a4334a12fc5d
邹景立 <zoujingli@qq.com>
2022-05-25 21:22:48 +0800

    更新编辑器

---
+++ public/static/plugs/ckeditor5/translations/ms.js
@@ -0,0 +1,1 @@
+!function(o){const e=o.ms=o.ms||{};e.dictionary=Object.assign(e.dictionary||{},{"%0 of %1":"",Cancel:"","Remove color":"","Restore default":"",Save:"","Show more items":""}),e.getPluralForm=function(o){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
