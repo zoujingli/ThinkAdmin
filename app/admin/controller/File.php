2544a30fcb4c01c478b0abbae0d8d88152a8c5e2
Anyon <zoujingli@qq.com>
2022-06-27 14:25:39 +0800

    修改文件管理

---
+++ app/admin/controller/File.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\admin\controller;
+
+use think\admin\Controller;
+use think\admin\helper\QueryHelper;
+use think\admin\model\SystemFile;
+
+/**
+ * 系统文件管理
+ * Class File
+ * @package app\admin\controller
+ */
+class File extends Controller
+{
+    /**
+     * 存储方式处理
+     * @var string[]
+     */
+    protected $types = [
+        'local'  => '本地服务器存储',
+        'qiniu'  => '七牛云对象存储',
+        'upyun'  => '又拍云USS存储',
+        'alioss' => '阿里云OSS存储',
+        'txcos'  => '腾讯云COS存储'
+    ];
+
+    /**
+     * 系统文件管理
+     * @auth true
+     * @menu true
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function index()
+    {
+        SystemFile::mQuery()->layTable(function () {
+            $this->title = '系统文件管理';
+        }, function (QueryHelper $query) {
+            $query->like('name,hash,xext')->dateBetween('create_at');
+        });
+    }
+
+    protected function _page_filter(&$data)
+    {
+        foreach ($data as &$vo) {
+            $vo['ctype'] = $this->types[$vo['type']] ?? $vo['type'];
+        }
+    }
+}
