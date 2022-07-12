24b17f900a03092fa14afd217f344915689106e1
Anyon <zoujingli@qq.com>
2022-07-12 11:30:20 +0800

    Update Upload.php

--- app/admin/controller/api/Upload.php
+++ app/admin/controller/api/Upload.php
@@ -153,7 +153,8 @@ class Upload extends Controller
         SystemFile::mQuery()->layTable(function () {
             $this->title = '文件选择器';
         }, function (QueryHelper $query) {
-            $query->like('name,hash')->dateBetween('create_at')->order('id desc');
+            $query->like('name,hash')->dateBetween('create_at');
+            $query->whereLike('mime', 'image/%')->order('id desc');
             $query->where(['status' => 2, 'issafe' => 0, 'uuid' => AdminService::getUserId()]);
         });
     }
