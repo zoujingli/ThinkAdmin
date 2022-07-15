200b503456717512421a6acb84dd723fc2770dd9
Anyon <zoujingli@qq.com>
2022-07-15 09:53:41 +0800

    修改layui分页

--- public/static/admin.js
+++ public/static/admin.js
@@ -751,17 +751,11 @@ $(function () {
             });
             // 实例并绑定事件
             $table.data('this', layui.table.render(bindData(option)));
-            $table.bind('reload render reloadData', function (evt, opts, next) {
+            $table.bind('reload render reloadData', function (evt, opts) {
                 data = $.extend({}, data, (opts || {}).where || {});
                 opts = bindData($.extend({}, opts || {}, {loading: true}));
                 if (evt.type.indexOf('reload') > -1) {
-                    if (layui.table.reloadData) {
-                        next = $table.data('next'), next.where = data;
-                        next.sort = next.initSort = opts.initSort || {};
-                        layui.table.reloadData(table.id, (opts.page || {}).curr || 1);
-                    } else {
-                        layui.table.reload(table.id, opts);
-                    }
+                    layui.table.reloadData(table.id, opts);
                 } else {
                     layui.table.render(table.id, opts);
                 }
