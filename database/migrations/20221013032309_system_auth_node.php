5900ae17d479fb931afba13c085e207534682fcd
邹景立 <zoujingli@qq.com>
2022-10-13 14:25:33 +0800

    Update 20221013032309_system_auth_node.php

--- database/migrations/20221013032309_system_auth_node.php
+++ database/migrations/20221013032309_system_auth_node.php
@@ -15,10 +15,10 @@ class SystemAuthNode extends Migrator
         $this->table($this->name, [
             'engine' => 'InnoDB', 'collation' => 'utf8mb4_general_ci', 'comment' => '系统-授权',
         ])
-            ->addColumn('auth', 'integer', ['limit' => 20, 'default' => 0, 'comment' => '角色'])
-            ->addColumn('node', 'string', ['limit' => 200, 'default' => '', 'comment' => '节点'])
-            ->addIndex('auth', ['name' => 'idx_system_auth_node_title'])
-            ->addIndex('auth', ['name' => 'idx_system_auth_node_status'])
+            ->addColumn('auth', 'integer', ['limit' => 20, 'default' => 0, 'comment' => '角色编号'])
+            ->addColumn('node', 'string', ['limit' => 200, 'default' => '', 'comment' => '节点路径'])
+            ->addIndex('auth', ['name' => 'idx_system_auth_node_auth'])
+            ->addIndex('node', ['name' => 'idx_system_auth_node_node'])
             ->save();
     }
 }
