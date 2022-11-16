d32c23432d6755d1a6dd52647c67dd1de65d1df0
邹景立 <zoujingli@qq.com>
2022-11-16 11:52:35 +0800

    同步配置

--- config/cache.php
+++ config/cache.php
@@ -33,5 +33,19 @@ return [
             // 序列化机制
             'serialize'  => [],
         ],
+        'safe' => [
+            // 驱动方式
+            'type'       => 'File',
+            // 缓存保存目录
+            'path'       => with_path('safefile/cache/'),
+            // 缓存名称前缀
+            'prefix'     => '',
+            // 缓存有效期 0 表示永久缓存
+            'expire'     => 0,
+            // 缓存标签前缀
+            'tag_prefix' => 'tag:',
+            // 序列化机制
+            'serialize'  => [],
+        ],
     ],
 ];
