49bdbeaa04f180927b8526c444e86d0e432e85de
邹景立 <zoujingli@qq.com>
2025-05-03 12:26:55 +0800

    同步更新插件代码

--- public/static/plugs/admin/queue.js
+++ public/static/plugs/admin/queue.js
@@ -1,7 +1,7 @@
 // +----------------------------------------------------------------------
 // | Static Plugin for ThinkAdmin
 // +----------------------------------------------------------------------
-// | 版权所有 2014~2024 ThinkAdmin [ thinkadmin.top ]
+// | 版权所有 2014~2025 ThinkAdmin [ thinkadmin.top ]
 // +----------------------------------------------------------------------
 // | 官方网站: https://thinkadmin.top
 // +----------------------------------------------------------------------
@@ -21,7 +21,7 @@ define(function () {
     function Queue(code, doScript, element) {
         let queue = this;
         (this.doAjax = true) && (this.doReload = false) || layer.open({
-            type: 1, title: false, area: ['560px', '315px'], anim: 2, shadeClose: false, end: function () {
+            type: 1, title: false, area: ['560px', '318px'], anim: 2, shadeClose: false, end: function () {
                 queue.doAjax = queue.doReload && doScript && $.layTable.reload(((element || {}).dataset || {}).tableId || true) && false;
             }, content: laytpl(template).render({code: code}), success: function ($elem) {
                 new Progress($elem, code, queue, doScript);
